b67dc7467a68205904e9e735fe416b9c8aa6e6f3
tasnimmizaoui <tasnimmizaoui0@gmail.com>
2026-01-31 18:01:22 +0100

    fix: Update S3 backend region to eu-north-1 and comment out dynamodb_table

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket         = "infraguard-terraform-state"  # From bootstrap output 
     key            = "infraguard/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-north-1"
     encrypt        = true
-    dynamodb_table = "infraguard-terraform-locks"  # From bootstrap output 
+    #dynamodb_table = "infraguard-terraform-locks"  # From bootstrap output 
   }
 }
