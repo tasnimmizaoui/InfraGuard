195e721e8bc10f31d5adfaf8ce68488ad408fdb9
tasnimmizaoui <tasnimmizaoui0@gmail.com>
2026-02-01 20:24:18 +0100

    fix: change region

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "infraguard-terraform-state"  # From bootstrap output 
     key            = "infraguard/terraform.tfstate"
-    region         = "eu-north-1"
+    region         = "us-east-1"
     encrypt        = true
     #dynamodb_table = "infraguard-terraform-locks"  # From bootstrap output 
   }
